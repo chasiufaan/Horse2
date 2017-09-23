d291ce3f7391454863074a8b542555cb8074285b
chasiufaan <wongd27@gmail.com>
2017-09-22 21:51:23 -0400

    adding documentations

--- src/ca/horse/config/AppConfig.java
+++ src/ca/horse/config/AppConfig.java
@@ -10,13 +10,21 @@ import org.springframework.http.converter.json.MappingJackson2HttpMessageConvert
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+/**
+ * This class is for configuring Spring MVC. There is no view here..sort of... the view here is the json returned
+ * 
+ * @author whkd2
+ *
+ */
 @Configuration
 @ComponentScan("ca.horse")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter{
 	
+	
 	@Override
 	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		//configures http response body to be json
 		converters.add(new MappingJackson2HttpMessageConverter());
 	}
 }
--- src/ca/horse/config/WebAppInitializer.java
+++ src/ca/horse/config/WebAppInitializer.java
@@ -2,19 +2,40 @@ package ca.horse.config;
 
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
+/**
+ * WebApplicationInitializer is an interface provided by Spring MVC that ensures your code-based configuration 
+ * is detected and automatically used to initialize any Servlet 3 container. An abstract base class implementation 
+ * of this interface named AbstractAnnotationConfigDispatcherServletInitializer makes it even easier to register 
+ * the DispatcherServlet by simply specifying its servlet mapping and listing configuration classes - it’s even 
+ * the recommended way to set up your Spring MVC application
+ * 
+ * Great Explanation on ApplicationContext Hierarchies:
+ * https://stackoverflow.com/questions/35258758/getservletconfigclasses-vs-getrootconfigclasses-when-extending-abstractannot
+ * 
+ * @author whkd2
+ *
+ */
 public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer{
 
+	//let's dump AppConfig.class in here since we only have one dispatcher servlet
 	@Override
 	protected Class<?>[] getRootConfigClasses() {
 		return new Class[] {AppConfig.class};
 	}
 
+	/**
+	 * this can return null if all configurations done through getRotConfigClasses
+	 */
 	@Override
 	protected Class<?>[] getServletConfigClasses() {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
 
+	/**
+	 * specifies servlet mappings here
+	 * Examples: "/example", "/example/ex/"
+	 */
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { 
